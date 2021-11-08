9880a02af0301de402ac0c770ff6ffdf7fc29a3a
ChampX5 <89368086+ChampX5@users.noreply.github.com>
2021-11-08 08:36:40 +0530

    Add files via upload

---
+++ src/app_class.py
@@ -0,0 +1,20 @@
+import pygame
+
+from settings import *
+
+
+class App:
+    
+    def __init__(self):
+
+        self.screen = pygame.display.update((WIDTH, HEIGHT))
+        self.running = True
+    
+    def run(self):
+
+        while self.running:
+
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    self.running = False
